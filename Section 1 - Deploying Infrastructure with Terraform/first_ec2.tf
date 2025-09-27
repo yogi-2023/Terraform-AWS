ef8221c48d00bb5930b081c6f79f3dfd2540ca26
yogi-2023 <159482503+yogi-2023@users.noreply.github.com>
2025-09-26 20:06:41 -0600

    Update first_ec2.tf

--- Section 1 - Deploying Infrastructure with Terraform/first_ec2.tf
+++ Section 1 - Deploying Infrastructure with Terraform/first_ec2.tf
@@ -1,1 +1,16 @@
+### first_ec2.tf
 
+```sh
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "XXXXX"
+  secret_key = "XXXXX"
+}
+resource "aws_instance" "Dev" {
+  ami           = "ami-XXXXXX"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "DEV-EC2"
+  }
+}
+```
