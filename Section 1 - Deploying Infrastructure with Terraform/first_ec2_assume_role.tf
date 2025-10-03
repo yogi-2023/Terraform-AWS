68d84a0345f0ac546a6f7369baadb89b5a848b66
yogi-2023 <159482503+yogi-2023@users.noreply.github.com>
2025-10-02 18:09:19 -0600

    Update first_ec2_assume_role.tf

--- Section 1 - Deploying Infrastructure with Terraform/first_ec2_assume_role.tf
+++ Section 1 - Deploying Infrastructure with Terraform/first_ec2_assume_role.tf
@@ -1,1 +1,18 @@
 
+
+provider "aws" {
+  region = "us-east-1"
+
+  assume_role {
+    role_arn     = "arn:aws:iam::XXXXXXXX:role/TerraformAssumeRole"
+    session_name = "terraform-session"
+  }
+}
+resource "aws_instance" "Dev" {
+  ami           = "ami-XXXXXXX"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "DEV-EC2"
+  }
+}
