69d563cab0ae11765dc9ea4e0462cb6e9743aad4
yogi-2023 <159482503+yogi-2023@users.noreply.github.com>
2025-10-09 16:14:55 -0600

    Update ec2tag.tf

--- Section 1 - Deploying Infrastructure with Terraform/ec2tag.tf
+++ Section 1 - Deploying Infrastructure with Terraform/ec2tag.tf
@@ -1,1 +1,30 @@
+#EC2Tag.tf
 
+provider "aws" {
+  region = "us-east-1"
+  assume_role {
+    role_arn     = "arn:aws:iam::XXXXXX:role/TerraformAssumeRole"
+    session_name = "terraform-session"
+  }
+}
+variable "common_tags" {
+  type = map(string)
+  default = {
+    Environment = "UAT"
+    Owner       = "Alice"
+    Project     = "ProjectA"
+    CostCenter  = "CC1001"
+    Department  = "Engineering"
+    Application = "WebApp"
+    Team        = "Backend"
+    Purpose     = "Testing"
+  }
+}
+resource "aws_instance" "UAT" {
+  ami           = "ami-XXXXXX"
+  instance_type = "t2.micro"
+  tags = merge(var.common_tags, {
+    Name = "UAT-EC2"
+    Type = "App"
+  })
+}
